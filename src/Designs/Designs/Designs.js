5d3a58e1bb2a78704d1cdfd603bc8bd95ad0602b
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2023-10-25 20:19:37 +0545

    value in text area creating problem while typing

--- src/Designs/Designs/Designs.js
+++ src/Designs/Designs/Designs.js
@@ -314,7 +314,7 @@ export default function Designs() {
                                                     clickedHTMLElement.textContent = el.target.value;
                                                 }
                                             }}
-                                            value={clickedHTMLElement.textContent}
+                                            placeholder={clickedHTMLElement.textContent}
                                         />
                                         :
                                         <input 
