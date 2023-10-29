be43c2887933053c475efe5764c537fb7671a1d0
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2023-10-30 01:06:46 +0545

    auto changed Extraction process

--- src/Designs/Designs/Designs.js
+++ src/Designs/Designs/Designs.js
@@ -71,7 +71,7 @@ export default function Designs() {
                                     <button
                                         className="rounded-md bg-indigo-600 px-5 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
                                         onClick={() => {
-                                            const html = document.getElementById("extract-code").innerHTML;
+                                            const html = document.getElementById("edit-space").innerHTML;
                                             dispatch(updateCode({html}));  
                                         }}
                                     >
--- src/Designs/Exports/Exports.js
+++ src/Designs/Exports/Exports.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Editor from '@monaco-editor/react';
+import Navigation from "../navigation/Navigation"
+
 
 export default function Exports() {
   const selectedCode = useSelector(state => state.update);
-  
+  const headersTags = ` <!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /></head><body>`
+  const bottomTags = `</body></html>`;
+
   return (
-    <div className='min-h-screen min-w-screen bg-slate-800 text-white' onLoad={() => alert("Asdf")}>
-      <Editor
-        height="80vh"
-        width="100%"
-        theme="vs-dark"
-        defaultLanguage="html"
-        defaultValue={selectedCode[0] }
-        onMount={(editor) => editor.trigger('editor', "editor.action.formatDocument")}     
-      />
+    <div className='min-h-screen min-w-screen bg-slate-800 text-white'>
+        <Navigation />
+      
+        <section>
+          <Editor
+            height="80vh"
+            width="100%"
+            theme="vs-dark"
+            defaultLanguage="html"
+            defaultValue={headersTags + selectedCode[0] + bottomTags}
+            onMount={(editor) => editor.trigger('editor', 'editor.action.formatDocument')}     
+          />
+        </section>
     </div>
   );
 }
-
