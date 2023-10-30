ed001a291353695fd84f5f1515616366dbf7d535
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2023-10-30 17:28:24 +0545

    editor page updated

--- src/Designs/Exports/Exports.js
+++ src/Designs/Exports/Exports.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Editor from '@monaco-editor/react';
 import Navigation from "../navigation/Navigation"
@@ -6,29 +6,33 @@ import Navigation from "../navigation/Navigation"
 
 export default function Exports() {
   const selectedCode = useSelector(state => state.codes);
-  const [codes, setCodes] = useState();
+  const [htmlCodes, setHtmlCodes] = useState();
+  const [cssCodes, setCssCodes] = useState();
+
   const headersTags = ` <!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /></head><body>`
   const bottomTags = `</body></html>`;
 
-  useLayoutEffect(() => {
-    selectedCode.forEach(el => {
-      // setCodes(codes + el.codeParams.html);
-    })
-    console.log(codes)
-  }, [selectedCode, codes]);
+  useEffect(() => {
+    const html = selectedCode.map(code => code.codeParams.html).join('');
+    setHtmlCodes(html);
+    
+    const css = selectedCode.map(code => code.codeParams.css).join('');
+    setCssCodes(css);
+
+  }, [selectedCode]);
+
 
   return (
     <div className='min-h-screen min-w-screen bg-slate-800 text-white'>
-        <Navigation />
-      <section className='flex justify-center space-x-2 items-center h-screen absolute top-0'>
-
-        <section>
+      <Navigation />
+      <section className='flex justify-center space-x-2 items-center h-screen'>
+        <section id='render-html-codes'>
           <Editor
             height="80vh"
             width="40vw"
             theme="vs-dark"
             defaultLanguage="html"
-            defaultValue={codes}
+            defaultValue={headersTags + htmlCodes + bottomTags}
             onMount={(editor) => {
               setTimeout(() => {
                 editor.trigger('editor', 'editor.action.formatDocument');
@@ -37,13 +41,13 @@ export default function Exports() {
           />
         </section>
 
-        <section>
+        <section id='render-css-codes'>
           <Editor
             height="80vh"
             width="40vw"
             theme="vs-dark"
             defaultLanguage="css"
-            defaultValue={selectedCode[0].codeParams.css}
+            defaultValue={cssCodes}
             onMount={(editor) => {
               setTimeout(() => {
                 editor.trigger('editor', 'editor.action.formatDocument');
@@ -52,7 +56,6 @@ export default function Exports() {
           />
         </section>
       </section>
-
     </div>
   );
 }
