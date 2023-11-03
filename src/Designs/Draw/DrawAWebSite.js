9c91e5cd3f70e3fc4bb72503845f4285c854a404
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2023-11-03 16:49:16 +0545

    added drawing pannel and drawing and moving machenism

--- src/Designs/Draw/DrawAWebSite.js
+++ src/Designs/Draw/DrawAWebSite.js
@@ -1,10 +1,112 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { FiSidebar } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 import testProfile from "../test-image/test-profile.jpeg"
 
 export default function DrawAWebSite() {
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [create, setCreate] = useState(false);
+
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+ 
+    const [store, setStore] = useState([]);
+
+    const [ctx, setCtx] = useState(); 
+    const [canvas, setCanvas] = useState();
+
+    useEffect(() => {
+        setCanvas(document.getElementById("canvas"));
+        setCtx(document.getElementById("canvas").getContext("2d"));
+    }, [canvas, ctx]);
+
+    function setPosition(e) {
+        canvas.style.zIndex = 1;
+        setIsDrawing(true);
+        setX(e.clientX - canvas.getBoundingClientRect().left);
+        setY(e.clientY - canvas.getBoundingClientRect().top);
+    }
+
+    function stopDrawing() {
+        // constantly change the button position
+        canvas.style.zIndex = -1;
+        setIsDrawing(false);
+
+        let storeTemp = { x, y, height, width };
+        setStore([...store, storeTemp]);
+
+        setX(0);
+        setY(0);
+        setHeight(0);
+        setWidth(0);
+
+        // create div based on the drawn design
+        let div = document.createElement("div");
+        div.style.position = "absolute";
+        div.style.top = storeTemp.y + "px";
+        div.style.left = storeTemp.x + "px";
+        div.style.height = storeTemp.height - storeTemp.y + "px";
+        div.style.width = storeTemp.width  - storeTemp.x + "px";
+        div.style.backgroundColor = "orange";
+        
+        document.body.appendChild(div);
+    }
+
+    function startDrawing(e) {
+        if (!isDrawing) return;
+
+        setWidth(e.clientX - canvas.getBoundingClientRect().left);
+        setHeight(e.clientY - canvas.getBoundingClientRect().top);
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.rect(x, y, width - x , height - y);
+        ctx.stroke();
+    }
+
+    
+    function clickedItem() {
+        const divs = document.querySelectorAll("div");
+
+        divs.forEach(element => {
+            element.addEventListener("click", () => {
+                move(element)
+            })
+        });
+    }
+
+    function move(element) {
+        let isDragging = false;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        element.addEventListener("mousedown", (e) => {
+            isDragging = true;
+            const initialLeft = parseFloat(getComputedStyle(element).left);
+            const initialTop = parseFloat(getComputedStyle(element).top);
+            const offsetX = e.clientX - initialLeft;
+            const offsetY = e.clientY - initialTop;
+
+            document.addEventListener("mousemove", (e) => {
+                if (isDragging) {
+                    const newX = e.clientX - offsetX;
+                    const newY = e.clientY - offsetY;
+                    element.style.left = newX + "px";
+                    element.style.top = newY + "px";
+                }
+            });
+
+            document.addEventListener("mouseup", () => {
+                isDragging = false;
+            });
+        });
+    }
+
+    function resize(element) {
+        
+    }
 
   return (
     <section>
@@ -61,7 +163,12 @@ export default function DrawAWebSite() {
             </section>} 
         </aside>
 
-        <canvas id='canvas' className="h-screen w-screen border-4 border-black" ></canvas>
+        <canvas id='canvas' height="1080" width="1920" className=" border-4 border-black"
+            onMouseDown={el => setPosition(el)}
+            onMouseUp={() => stopDrawing()}
+            onMouseMove={(el) => startDrawing(el)}
+            onClick={() => clickedItem()}
+        ></canvas>
         
     </section>
   )
