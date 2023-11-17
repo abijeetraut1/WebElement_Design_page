5e3109846f4af465de969c46165719bf1eac4810
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2023-11-17 19:51:52 +0545

    added text editor

--- src/Designs/Designs/Functions/popupEditor/PopupElement.js
+++ src/Designs/Designs/Functions/popupEditor/PopupElement.js
@@ -15,97 +15,100 @@ export default function PopupElement(clickedHTMLElement) {
     const dispatch = useDispatch();
 
     return (
-        <section id='editor-container' className={`absolute hidden content-center space-x-1 `}>
-            <div className='flex content-center cursor-move'>
-                <button>
-                    <VscGripper className='text-2xl' />
-                </button>
-            </div>
+        <section>
+            <section id='editor-container' className={`absolute hidden content-center space-x-1 `}>
+                <div className='flex content-center cursor-move'>
+                    <button>
+                        <VscGripper className='text-2xl' />
+                    </button>
+                </div>
 
-            <div className='px-3'>
-                <input className='scale-150' type="color" name="fontColorChange" id="fontColorChange" 
-                    onChange={(event) => PopupButtons(clickedHTMLElement, event, "fontColor")}
-                />
-            </div>
+                <div className='px-3'>
+                    <input className='scale-150' type="color" name="fontColorChange" id="fontColorChange" 
+                        onChange={(event) => PopupButtons(clickedHTMLElement, event, "fontColor")}
+                    />
+                </div>
 
-            <div>
-                <button
-                    className="rounded-md bg-indigo-600 px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
-                    onClick={(event) => PopupButtons(clickedHTMLElement,event, "bold")}
-                >
-                    <VscBold />
-                </button>
-            </div>
-            <div>
-                <button
-                    onClick={(event) => PopupButtons(clickedHTMLElement, event, "italic")}
-                    className="rounded-md bg-indigo-600 px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
-                >
-                    <VscItalic />
-                </button>
-            </div>
-            <div>
-                <select name="" id="fonts" 
-                    style={{outline:"none"}}
-                    onChange={(event) => {
-                        dispatch(storeFonts(event.target.value))
-                        clickedHTMLElement.clicked.style.fontFamily = event.target.value;
-                        setChoosenFont(event.target.value);
-                    }}
-                    className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
-                >
-                    {fontsProtected && <h3 className="text-white" >Please Wait Slow Internet.</h3>}
-                    {fontsExtractError && <h3 className="text-white" >Server Down Please Wait😥 Devs are working on it.</h3>}
-                    {fonts && fonts.data.items.map((font, i) => (
-                        <>
-                            {i <= 50 ? <option value={font.family}  style={{fontFamily : font.family}}>{font.family}</option> : ""}
-                        </>
-                    ))}
-                </select>
-            </div>
-            <div>
-                <select name="" id="fonts" 
-                    style={{outline:"none"}}
-                    onChange={(event) => PopupButtons(clickedHTMLElement, event, "fontWeight")}
-                    className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
-                >
-                    {fontsVarianceProtected && <h3 className="text-white" >Please Wait Slow Internet.</h3>}
-                    {fontsVarianceExtractError && <h3 className="text-white" >Server Down Please Wait😥 Devs are working on it.</h3>}
-                    {fontsVariance && fontsVariance.data.items[0].variants.map((variance, i) => (
-                      <>
-                        <option value={variance} >{variance}</option>
-                      </>
-                    ))}
-                </select>
-            </div>
-            <div>
-                <select name="" id="" 
-                    style={{outline:"none"}}
-                    className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
-                    onChange={(event) => PopupButtons(clickedHTMLElement, event, "fontSize")}
-                >
-                    <option value="10px">10 px</option>
-                    <option value="11px">11 px</option>
-                    <option value="12px">12 px</option>
-                    <option value="13px">13 px</option>
-                    <option value="14px">14 px</option>
-                    <option value="15px">15 px</option>
-                    <option value="16px">16 px</option>
-                    <option value="20px">20 px</option>
-                    <option value="24px">24 px</option>
-                    <option value="32px">32 px</option>
-                    <option value="40px">40 px</option>
-                    <option value="48px">48 px</option>
-                    <option value="52px">52 px</option>
-                    <option value="64px">64 px</option>
-                    <option value="72px">72 px</option>
-                    <option value="84px">84 px</option>
-                    <option value="96px">96 px</option>
-                    <option value="108px">108 px</option>
-                </select>
-            </div>
+                <div>
+                    <button
+                        className="rounded-md bg-indigo-600 px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
+                        onClick={(event) => PopupButtons(clickedHTMLElement,event, "bold")}
+                    >
+                        <VscBold />
+                    </button>
+                </div>
+                <div>
+                    <button
+                        onClick={(event) => PopupButtons(clickedHTMLElement, event, "italic")}
+                        className="rounded-md bg-indigo-600 px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
+                    >
+                        <VscItalic />
+                    </button>
+                </div>
+                <div>
+                    <select name="" id="fonts" 
+                        style={{outline:"none"}}
+                        onChange={(event) => {
+                            dispatch(storeFonts(event.target.value))
+                            clickedHTMLElement.clicked.style.fontFamily = event.target.value;
+                            setChoosenFont(event.target.value);
+                        }}
+                        className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
+                    >
+                        {fontsProtected && <h3 className="text-white" >Please Wait Slow Internet.</h3>}
+                        {fontsExtractError && <h3 className="text-white" >Server Down Please Wait😥 Devs are working on it.</h3>}
+                        {fonts && fonts.data.items.map((font, i) => (
+                            <>
+                                {i <= 50 ? <option value={font.family}  style={{fontFamily : font.family}}>{font.family}</option> : ""}
+                            </>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    <select name="" id="fonts" 
+                        style={{outline:"none"}}
+                        onChange={(event) => PopupButtons(clickedHTMLElement, event, "fontWeight")}
+                        className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
+                    >
+                        {fontsVarianceProtected && <h3 className="text-white" >Please Wait Slow Internet.</h3>}
+                        {fontsVarianceExtractError && <h3 className="text-white" >Server Down Please Wait😥 Devs are working on it.</h3>}
+                        {fontsVariance && fontsVariance.data.items[0].variants.map((variance, i) => (
+                          <>
+                            <option value={variance} >{variance}</option>
+                          </>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    <select name="" id="" 
+                        style={{outline:"none"}}
+                        className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
+                        onChange={(event) => PopupButtons(clickedHTMLElement, event, "fontSize")}
+                    >
+                        <option value="10px">10 px</option>
+                        <option value="11px">11 px</option>
+                        <option value="12px">12 px</option>
+                        <option value="13px">13 px</option>
+                        <option value="14px">14 px</option>
+                        <option value="15px">15 px</option>
+                        <option value="16px">16 px</option>
+                        <option value="20px">20 px</option>
+                        <option value="24px">24 px</option>
+                        <option value="32px">32 px</option>
+                        <option value="40px">40 px</option>
+                        <option value="48px">48 px</option>
+                        <option value="52px">52 px</option>
+                        <option value="64px">64 px</option>
+                        <option value="72px">72 px</option>
+                        <option value="84px">84 px</option>
+                        <option value="96px">96 px</option>
+                        <option value="108px">108 px</option>
+                    </select>
+                </div>
 
 
+            </section>
         </section>
+
     )
 }
--- src/pages/UploadCode/Uploadcode.js
+++ src/pages/UploadCode/Uploadcode.js
@@ -9,73 +9,88 @@ export default function Uploadcode() {
     <div>
       <section className="bg-neutral-900 h-screen w-screen overflow-hidden">
         <Navigation />
-        <section className="flex justify-around items-center mt-4" >
-            <div>
-                <div className="flex space-x-2 border border-neutral-800 w-fit pl-2 pr-6 bg-neutral-800">
+        <section className="mx-10">
+            <section className="flex justify-end mt-4">
+                <div>
+                    <button
+                        className="rounded-md bg-indigo-600 px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
+                    >Save</button>
+                </div>
+            </section>
+            <section className="flex justify-between items-center mt-4" >
+
+                <div>
+                    <div className="flex space-x-2 border border-neutral-800 w-fit pl-2 pr-6 bg-neutral-800">
+                        <div>
+                            <span className="text-white">index.html</span>
+                        </div>
+                        <div className="flex items-center">
+                            <button>
+                                <VscClose className="text-white"/>
+                            </button>
+                        </div>  
+                    </div>
                     <div>
-                        <span className="text-white">index.html</span>
-                    </div>
-                    <div className="flex items-center">
-                        <button>
-                            <VscClose className="text-white"/>
-                        </button>
-                    </div>  
-                </div>
-                <div>
-                    <Editor
-                        height="80vh"
-                        width="30vw"
-                        theme="vs-dark"
-                        defaultLanguage="HTML"
-                    />
-                </div>
-            </div>
-            
-            <div>
-                <div className="flex space-x-2 border border-neutral-800 w-fit pl-2 pr-6 bg-neutral-800">
-                    <div className="flex space-x-2 ">
-                        <div>
-                            <span className="text-white">style.css</span>
-                        </div>
-                        <div className="flex items-center">
-                            <button><VscClose className="text-white"/></button>
-                        </div>
+                        {/* <Editor
+                            height="80vh"
+                            width="30vw"
+                            theme="vs-dark"
+                            defaultLanguage="HTML"
+                        /> */}
+                        <textarea className="bg-neutral-800 text-white" name="" id="" cols="80" rows="30" style={{resize: "none"}}></textarea>
                     </div>
                 </div>
 
                 <div>
-                    <Editor
-                        height="80vh"
-                        width="30vw"
-                        theme="vs-dark"
-                        defaultLanguage="css"
-                    />
-                </div>
-            </div>
-            
-
-            <div>
-                <div className="flex space-x-2 border border-neutral-800 w-fit pl-2 pr-6 bg-neutral-800">
-                    <div className="flex space-x-2 ">
-                        <div>
-                            <span className="text-white">script.js</span>
-                        </div>
-                        <div className="flex items-center">
-                            <button className="text-white"><VscClose className="text-white"/></button>
+                    <div className="flex space-x-2 border border-neutral-800 w-fit pl-2 pr-6 bg-neutral-800">
+                        <div className="flex space-x-2 ">
+                            <div>
+                                <span className="text-white">style.css</span>
+                            </div>
+                            <div className="flex items-center">
+                                <button><VscClose className="text-white"/></button>
+                            </div>
                         </div>
                     </div>
+
+                    <div>
+                        {/* <Editor
+                            height="80vh"
+                            width="30vw"
+                            theme="vs-dark"
+                            defaultLanguage="css"
+                        /> */}
+                        <textarea className="bg-neutral-800 text-white" name="" id="" cols="80" rows="30" style={{resize: "none"}}></textarea>
+
+                    </div>
                 </div>
+
+
                 <div>
-                    <Editor
-                        height="80vh"
-                        width="30vw"
-                        theme="vs-dark"
-                        defaultLanguage="javascript"
-                    />
+                    <div className="flex space-x-2 border border-neutral-800 w-fit pl-2 pr-6 bg-neutral-800">
+                        <div className="flex space-x-2 ">
+                            <div>
+                                <span className="text-white">script.js</span>
+                            </div>
+                            <div className="flex items-center">
+                                <button className="text-white"><VscClose className="text-white"/></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        {/* <Editor
+                            height="80vh"
+                            width="30vw"
+                            theme="vs-dark"
+                            defaultLanguage="javascript"
+                        /> */}
+                        <textarea className="bg-neutral-800 text-white" name="" id="" cols="80" rows="30" style={{resize: "none"}}></textarea>
+
+                    </div>
                 </div>
-            </div>
+            </section>
         </section>
-      </section>
+    </section>
     </div>
   )
 }
