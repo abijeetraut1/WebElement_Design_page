44c32b347c5fcd7daecfa1ad67fcfbbdea75fefa
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2023-11-17 22:38:52 +0545

    added file choosing facility

---
+++ src/pages/UploadCode/SlideComponent/SlideComponent.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { FiSidebar } from 'react-icons/fi'
+import html from "../../../Images/html.png"
+import css from "../../../Images/css.png"
+import js from "../../../Images/js.png"
+
+
+export default function SlideComponent() {
+    const [active, setActive] = useState("js");
+  return (
+    <section className='w-1/5 h-screen bg-neutral-800'>
+        <div className='space-x-2 p-2'>
+            <button
+                className='border:solid border border-white bg-zinc-900 p-2 rounded-md'
+            >
+                {<FiSidebar className= 'text-white text-xl ' />}
+            </button>
+            <span className='font-bold text-white'>Upload Code</span>
+        </div>
+        <hr className='border-neutral-600'/>
+        <section className='py-4 m-1 outline-none'>
+            <div 
+                onClick={() => setActive("html")}
+                className={`flex items-center space-x-3 px-3 py-1 cursor-pointer ${active === "html" ? "border border-neutral-900 bg-neutral-900 rounded" : ""} duration-300`}>
+                <div>
+                    <img className='h-6 w-6 rounded' src={html} alt="" />
+                </div>
+                <div>
+                    <button className='text-white'>index.html</button>
+                </div>
+            </div>
+            <div
+                onClick={() => setActive("css")}
+                className={`flex items-center space-x-3 px-3 py-1 cursor-pointer ${active === "css" ? "border border-neutral-900 bg-neutral-900 rounded" : ""} duration-300`}>
+                <div >
+                    <img className='h-6 w-6 rounded' src={css} alt="" /> 
+                </div>
+                <div>
+                    <button className='text-white'>style.css</button>
+                </div>
+            </div>
+            <div 
+                onClick={() => setActive("js")}
+                className={`flex items-center space-x-3 px-3 py-1 cursor-pointer ${active === "js" ? "border border-neutral-900 bg-neutral-900 rounded" : ""} duration-300`}>
+                <div>
+                    <img className='h-6 w-6 rounded' src={js} alt="" />
+                </div>
+                <div>
+                    <button className='text-white'>script.js</button>
+                </div>
+            </div>
+        </section>
+    </section>
+  )
+}
--- src/pages/UploadCode/Uploadcode.js
+++ src/pages/UploadCode/Uploadcode.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react"
 import Navigation from "../../Designs/navigation/Navigation"
-// import { VscClose } from "react-icons/vsc";
 import "./../../Designs/Designs/Functions/popupEditor/popupElement.css"
-import { saveCode } from "./Controller/SaveCodes";
+import SlideComponent from "./SlideComponent/SlideComponent";
 
 
 export default function Uploadcode() {
@@ -12,54 +11,24 @@ export default function Uploadcode() {
     
   return (
     <div>
-      <section className="bg-neutral-900 h-screen w-screen overflow-hidden">
+      <section className="bg-neutral-900 h-screen w-screen">
         <Navigation />
-        <section className="mx-10">
-            <section className="flex justify-end mt-4 space-x-2">
-                <div>
-                    <input type="file" name="" id="" 
-                        title="yo file"
-                        className="rounded-md bg-indigo-600 px-3 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
-                    />
+        <section className="flex space-x-10 w-screen">
+            <SlideComponent />
+            <div className="w-screen">
+                <div className="py-5">
+                    <button className="flex flex-col">
+                        <input type="file" name="" id="upload_file" className="text-transparent"/>
+                    </button>
                 </div>
                 <div>
-                    <button
-                        onClick={() => saveCode(html, css, js)}
-                        className="rounded-md bg-indigo-600 px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
-                    >Save</button>
+                    <textarea
+                        onChange={(event) => setHtml(event.target.value)}
+                        className="resize-none bg-neutral-800 text-white w-11/12 h-screen"
+                        name="" id=""
+                    ></textarea>
                 </div>
-            </section>
-            <section className="flex justify-between items-center mt-4" >
-
-                <div>
-                    <div>
-                        <textarea
-                            onChange={(event) => setHtml(event.target.value)}
-                            className="bg-neutral-800 text-white" name="" id="" cols="80" rows="30" style={{resize: "none"}}
-                        ></textarea>
-                    </div>
-                </div>
-
-                <div>
-                    <div>
-                        <textarea
-                            onChange={(event) => setCss(event.target.value)}
-                            className="bg-neutral-800 text-white" name="" id="" cols="80" rows="30" style={{resize: "none"}}
-                        ></textarea>
-
-                    </div>
-                </div>
-
-
-                <div>
-                    <div>
-                        <textarea
-                            onChange={(event) => setJs(event.target.value)}
-                            className="bg-neutral-800 text-white" name="" id="" cols="80" rows="30" style={{resize: "none"}}
-                        ></textarea>
-                    </div>
-                </div>
-            </section>
+            </div>
         </section>
     </section>
     </div>
