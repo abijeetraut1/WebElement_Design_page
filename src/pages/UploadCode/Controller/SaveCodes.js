60b81ad03e57864ab356c00b8f632e076cd6f69a
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2023-11-20 19:43:48 +0545

    added notifications

--- src/pages/UploadCode/Controller/SaveCodes.js
+++ src/pages/UploadCode/Controller/SaveCodes.js
@@ -1,5 +1,21 @@
-export function saveCode(html, css, js) {
+import axios from "axios";
+
+export async function saveCode(htmlCode, cssCode, js) {
   // Access environment variables using process.env
-  console.log(html, css, js);
-  console.log(process.env.REACT_APP_GOOGLE_FONT_API_KEY);
+  try {
+
+    const sendCode = await axios({
+      method: 'POST',
+      url: process.env.REACT_APP_UPLOAD_CODE_URL,
+      data: {
+        name: "helloworld",
+        htmlCode,
+        cssCode,
+        section: "body"
+      }
+    })
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
--- src/pages/UploadCode/Uploadcode.js
+++ src/pages/UploadCode/Uploadcode.js
@@ -1,18 +1,24 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Navigation from "../../Designs/navigation/Navigation"
 import "./../../Designs/Designs/Functions/popupEditor/popupElement.css"
-import { VscCheck } from "react-icons/vsc";
+import { VscCheck, VscClose } from "react-icons/vsc";
 import html from "../../Images/html.png"
 import css from "../../Images/css.png"
 import js from "../../Images/js.png"
+import { saveCode } from "./Controller/SaveCodes";
 
 export default function Uploadcode() {
-    const [htmlCode, setHtml] = useState(null);
-    const [cssCode, setCss] = useState(null);
-    const [jsCode, setJs] = useState(null);
+    const [htmlCode, setHtml] = useState("html");
+    const [cssCode, setCss] = useState("css");
+    const [jsCode, setJs] = useState("js");
     const [active, setActive] = useState("html");
+    const [notificationActive, setNotificationActive] = useState(true);
 
-
+    useEffect(() => {
+        setTimeout(() => {
+            setNotificationActive(false);
+        }, 3000)
+    }, [notificationActive]);
 
     return (
         <div>
@@ -60,21 +66,17 @@ export default function Uploadcode() {
 
                     <div className="w-11/12 pr-10">
                         <section className="my-2 flex justify-between">
-                            <div className="flex justify-start text-white space-x-1 items-center">
+                            <div className="flex text-white space-x-1 items-center">
                                 <span className="text-xl text-fontBlue">user</span>
                                 <span className="text-xl">/</span>
                                 <span><input className="bg-transparent border border-borderColor p-1 rounded w-fit h-8 outline-none focus:ring focus:ring-fontBlue focus:outline-none" type="text" name="" id="" /></span>
                                 <span>{<VscCheck className="text-xl text-fontBlue duration-200" />}</span>
                             </div>
-                            <div className="flex items-center">
-                                <div>
-                                    <button>
-                                        <input type="file" name="" id="" />
-                                    </button>
-                                </div>
+                            <div className="flex items-center space-x-2">
                                 <div>
                                     <button
-                                        className="rounded-md bg-indigo-600 px-5 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        className="rounded-md bg-green-900 px-5 py-3 text-sm font-semibold text-white shadow-sm hover:bg-emerald-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        onClick={() => saveCode(htmlCode, cssCode, jsCode)}
                                     >
                                         Upload
                                     </button>
@@ -85,29 +87,32 @@ export default function Uploadcode() {
                             <div>
                                 <div>
                                     <textarea
-                                        className={`border-2 p-2 w-full rounded resize-none border-neutral-800 text-white w-inherit ${active === "html" ? "block" : "hidden"} bg-inherit`}
+                                        className={`border-2 outline-none p-2 w-full rounded resize-none border-neutral-800 text-white w-inherit ${active === "html" ? "block" : "hidden"} bg-inherit`}
                                         name=""
                                         id="textarea"
                                         rows="30"
                                         onChange={(event) => setHtml(event.target.value)}
+                                        value={htmlCode}
                                     ></textarea>
                                 </div>
                                 <div>
                                     <textarea
-                                        className={`border-2 p-2 w-full rounded resize-none border-neutral-800 text-white w-inherit ${active === "css" ? "block" : "hidden"} bg-inherit`}
+                                        className={`border-2 outline-none p-2 w-full rounded resize-none border-neutral-800 text-white w-inherit ${active === "css" ? "block" : "hidden"} bg-inherit`}
                                         name=""
                                         id="textarea"
                                         rows="30"
                                         onChange={(event) => setCss(event.target.value)}
+                                        value={cssCode}
                                     ></textarea>
                                 </div>
                                 <div>
                                     <textarea
-                                        className={`border border-custom-dark outline-hidden p-2 w-full rounded resize-none text-white w-inherit ${active === "js" ? "block" : "hidden"} bg-inherit`}
+                                        className={`border-2 outline-none p-2 w-full rounded resize-none border-neutral-800 text-white w-inherit ${active === "js" ? "block" : "hidden"} bg-inherit`}
                                         name=""
                                         id="textarea"
                                         rows="30"
                                         onChange={(event) => setJs(event.target.value)}
+                                        value={jsCode}
                                     ></textarea>
                                 </div>
                             </div>
@@ -116,6 +121,15 @@ export default function Uploadcode() {
                     </div>
                 </section>
             </section>
+
+            {notificationActive &&
+                <div className="absolute top-2 right-2 text-white  flex items-center justify-center ">
+                    <div className="bg-green-900 flex items-center justify-center space-x-10 p-4 rounded">
+                        <div><span > {notificationActive === true ? "code saved" : "something went wrong"}</span></div>
+                        <div><button onClick={() => setNotificationActive(false)}><VscClose /></button></div>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
